f45aef67d2afbb33b09cbeea29b633710b808534
Thomas Cheng <thomascheng1998@googlemail.com>
2017-06-15 01:54:09 +0100

    Created gradle project, added slf4j logger.

---
+++ src/main/java/com/tom/draughts/Main.java
@@ -0,0 +1,11 @@
+package com.tom.draughts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  public static void main(String[] args) {
+    final Logger logger = LoggerFactory.getLogger(Main.class);
+    logger.debug("This is a debug message");
+  }
+}
